6173973634a3a44509aca42f2c24f1930295369f
George <dit18066@go.uop.gr>
2025-04-02 22:01:08 +0300

    Enhance Rock-Paper-Scissors game with detailed statistics and last game tracking

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ const numberOfRounds = document.getElementById("rounds");
 const last5Games = document.getElementById("last5Games");
 const numberOfWins = document.getElementById("winStat");
 const numberOfTies = document.getElementById("tieStat");
+const numberOfLosses = document.getElementById("loseStat");
 const frequentChoices = document.getElementById("freqchoice");
+const winPercent = document.getElementById("winPercent");
+const losePercent = document.getElementById("losePercent");
+const tiePercent = document.getElementById("tiePercent");
 
 let playerScore = 0;
 let computerScore = 0;
@@ -39,6 +43,8 @@ choiceBtns.forEach((button) => {
     round++;
     numberOfRounds.textContent = round;
     Last_5_Games(result, last5Games);
+    Statistics(result, round);
+    FreqChoice(playerChoice, round);
   });
 });
 
@@ -59,26 +65,66 @@ let counter = 0;
 
 function Last_5_Games(result, last5Games) {
   if (counter == 0) {
-    if (result.includes("You win")) last5Games.textContent = "W";
-    if (result.includes("Computer wins")) last5Games.textContent = "L";
+    if (result.includes("You win!")) last5Games.textContent = "W";
+    if (result.includes("Computer wins!")) last5Games.textContent = "L";
     if (result.includes("It's a tie!")) last5Games.textContent = "T";
     counter++;
   } else if (counter == 5) {
-    if (result.includes("You win"))
+    if (result.includes("You win!"))
       last5Games.textContent = "W - " + last5Games.textContent;
-    if (result.includes("Computer wins"))
+    if (result.includes("Computer wins!"))
       last5Games.textContent = "L - " + last5Games.textContent;
     if (result.includes("It's a tie!"))
       last5Games.textContent = "T - " + last5Games.textContent;
     last5Games.textContent = last5Games.textContent.slice(0, -4);
   } else {
-    if (result.includes("You win"))
+    if (result.includes("You win!"))
       last5Games.textContent = "W - " + last5Games.textContent;
-    if (result.includes("Computer wins"))
+    if (result.includes("Computer wins!"))
       last5Games.textContent = "L - " + last5Games.textContent;
     if (result.includes("It's a tie!"))
       last5Games.textContent = "T - " + last5Games.textContent;
     if (counter < 5) counter++;
   }
   console.log(counter);
+}
+
+let wins = 0;
+let lose = 0;
+let tie = 0;
+
+// THIS DOESN'T WORK CORRECTLY
+function Statistics(result, round) {
+  if (result.includes("You win!")) {
+    numberOfWins.textContent = ++wins;
+    winPercent.textContent = Math.round((wins / round) * 100);
+  }
+  if (result.includes("Computer wins!")) {
+    numberOfLosses.textContent = ++lose;
+    losePercent.textContent = Math.round((lose / round) * 100);
+  }
+  if (result.includes("It's a tie!")) {
+    numberOfTies.textContent = ++tie;
+    tiePercent.textContent = Math.round((tie / round) * 100);
+  }
+}
+
+let rock = 0;
+let paper = 0;
+let scissor = 0;
+
+function FreqChoice(player, round) {
+  if (player === "✊") ++rock;
+  if (player === "✋") ++paper;
+  if (player === "✌") ++scissor;
+  if (rock > paper && rock > scissor)
+    frequentChoices.textContent = `Rock, ${Math.round((rock / round) * 100)}%`;
+  else if (paper > scissor)
+    frequentChoices.textContent = `Paper, ${Math.round(
+      (paper / round) * 100
+    )}%`;
+  else
+    frequentChoices.textContent = `Scissors, ${Math.round(
+      (scissor / round) * 100
+    )}%`;
 }
